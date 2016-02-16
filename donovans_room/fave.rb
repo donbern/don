c9dfaddc08a7d21d1e114b820d8d2d07709b09fe
donbern <donovan_bernard@att.net>
2016-02-16 20:43:39 +0000

    first commit

---
+++ donovans_room/fave.rb
@@ -0,0 +1,10 @@
+print "what's your first name?"
+first_name = gets.chom
+
+print "What's your last name?"
+last_name = gets.chomp
+
+print "Who's your favorite performer or act?"
+performer = gets.chomp
+
+print "my name #{first_name} #{last_name}.
